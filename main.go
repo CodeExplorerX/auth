91618c65e76f651216e479eab4b961e3e586301a
= <=>
2024-11-13 14:59:13 +0100

    registration

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Login struct {
+	HashedPassword string
+	SessionToken   string
+	CSRFToken      string
+}
+
+var users = map[string]Login{}
+
+func main() {
+	http.HandleFunc("/register", register)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/logout", logout)
+	http.HandleFunc("/protected", protected)
+
+	http.ListenAndServe(":80", nil)
+}
+
+func register(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invaild method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+	if len(username) < 8 || len(password) < 8 {
+		http.Error(w, "Invalid username/password", http.StatusNotAcceptable)
+		return
+	}
+
+	if _, ok := users[username]; ok {
+		http.Error(w, "User already exists", http.StatusConflict)
+		return
+	}
+
+	hashedPassword, _ := hashPassword(password)
+	users[username] = Login{
+		HashedPassword: hashedPassword,
+	}
+
+	fmt.Fprintf(w, "User registered successfully!")
+}
+
+func login(w http.ResponseWriter, r *http.Request) {}
+
+func logout(w http.ResponseWriter, r *http.Request) {}
+
+func protected(w http.ResponseWriter, r *http.Request) {}
